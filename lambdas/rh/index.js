dca260b52f8a25985a59ef88ca430e9ec6aef61f
GitThePower <henrypower11@gmail.com>
2021-07-24 15:11:49 -0400

    Adding manual login functionality

--- lambdas/rh/index.js
+++ lambdas/rh/index.js
@@ -1,6 +1,33 @@
+const rh = require('robinhood');
+const { config } = require('../../local-config');
+
+let authToken = null;
+const credentials = {
+    username: config.rhUsername,
+    password: config.rhPassword
+};
+
 exports.handler = async () => {
+    if (authToken) {
+        credentials.token = authToken;
+        delete credentials.username;
+        delete credentials.password;
+    }
+
+    let Robinhood = rh(credentials, (data) => {
+        if (data && data.mfa_required) {
+            let mfa_code = '';
+            Robinhood.set_mfa_code(mfa_code, () => {
+                console.log(Robinhood.auth_token());
+            })
+        } else {
+            Robinhood.get_crypto('BTC', (err, res, body) => {
+                console.log(body);
+            });
+        }
+    })
+
     return {
-        message: 'Success',
-        statusCode: 200
+        message: 'Success'
     };
 }
