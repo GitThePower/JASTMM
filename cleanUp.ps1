2a29938b9b6f8f7988feac1c694c4ea0fd71a16e
GitThePower <henrypower11@gmail.com>
2021-10-11 22:45:37 -0400

    Updating dependencies and cleanup

--- cleanUp.ps1
+++ cleanUp.ps1
@@ -2,3 +2,11 @@ Get-ChildItem bin -Recurse | Where{$_.Name -Match ".+\.(d\.t|j)s"} | Remove-Item
 Get-ChildItem lib -Recurse | Where{$_.Name -Match ".+\.(d\.t|j)s"} | Remove-Item
 Get-ChildItem local-config -Recurse | Where{$_.Name -Match ".+\.(d\.t|j)s"} | Remove-Item
 Get-ChildItem test/lib -Recurse | Where{$_.Name -Match ".+\.(d\.t|j)s"} | Remove-Item
+Get-ChildItem cdk.out -Recurse | Remove-Item -Recurse -Force -Confirm:$false
+if (Test-Path -Path cdk.out) {
+    Remove-Item cdk.out
+}
+Get-ChildItem coverage -Recurse | Remove-Item -Recurse -Force -Confirm:$false
+if (Test-Path -Path coverage) {
+    Remove-Item coverage
+}
