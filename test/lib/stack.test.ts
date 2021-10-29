eadb9fdcc5286fc3b2e326fa20a924d878a2f4fa
GitThePower <henrypower11@gmail.com>
2021-10-28 22:40:01 -0400

    Updating tests to match resource creation

--- test/lib/stack.test.ts
+++ test/lib/stack.test.ts
@@ -21,7 +21,9 @@ test('Jastmm stack should create resources', () => {
   expectCDK(stack).to(countResources("AWS::IAM::Role", 1));
   expectCDK(stack).to(countResources("AWS::Lambda::EventInvokeConfig", 1));
   expectCDK(stack).to(countResources("AWS::Lambda::Function", 1));
-  expectCDK(stack).to(countResources("AWS::SecretsManager::Secret", 1));
+  expectCDK(stack).to(countResources("AWS::Lambda::Permission", 1));
+  expectCDK(stack).to(countResources("AWS::SNS::Subscription", 1));
   expectCDK(stack).to(countResources("AWS::SNS::Topic", 1));
   expectCDK(stack).to(countResources("AWS::SNS::TopicPolicy", 1));
+  expectCDK(stack).to(countResources("AWS::SecretsManager::Secret", 1));
 });
