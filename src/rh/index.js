34798e16945c8bf9fa57d3aeeca6540d5d32ec35
GitThePower <henrypower11@gmail.com>
2021-11-02 23:19:39 -0400

    Reworking rhLambda logging a bit

--- src/rh/index.js
+++ src/rh/index.js
@@ -2,19 +2,18 @@ const AWS = require('aws-sdk');
 let credentials = null;
 
 exports.handler = async (event) => {
-  const startUpMsg = (event) ? 'sns event received.' : 'starting scheduled exection.';
-  console.log(`func=rhLambda,msg=${startUpMsg}`);
-  
+  console.log('func=rhLambda,msg=starting exection');
   const SM = new AWS.SecretsManager();
-
   if (!credentials) {
     try {
       const secretPromise = await SM.getSecretValue({ SecretId: process.env.RH_CREDENTIALS_ARN }).promise();
       credentials = JSON.parse(secretPromise.SecretString);
     } catch {
-      throw new Error('Unable to get Secret Value.');
+      const errorMsg = 'unable to get secret value'
+      console.error(`func=rhLambda,msg=${errorMsg}`);
+      throw new Error(errorMsg);
     }
   }
-
-  return credentials;
+  
+  console.log('func=rhLambda,msg=success');
 }
