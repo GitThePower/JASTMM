08630a2b6ca40e9ab7367bde4257f9a3fc9115b4
GitThePower <henrypower11@gmail.com>
2021-11-02 23:41:12 -0400

    Adding parsing for sns events

--- src/rh/index.js
+++ src/rh/index.js
@@ -1,8 +1,28 @@
 const AWS = require('aws-sdk');
 let credentials = null;
 
+const tryParseSNSEvent = (event) => {
+  if(event &&
+    event.Records &&
+    event.Records[0] &&
+    event.Records[0].Sns &&
+    event.Records[0].Sns.Message) {
+      try {
+        const data = JSON.parse(event.Records[0].Sns.Message);
+        return data.messageBody;
+      } catch {
+        console.error(`func=rhLambda,msg=invalid sns event`);
+      }
+  }
+
+  return null;
+}
+
 exports.handler = async (event) => {
-  console.log('func=rhLambda,msg=starting exection');
+  const eventMessage = tryParseSNSEvent(event);
+  const startMsg = (eventMessage) ? 'sns event received' : 'scheduled execution';
+  console.log(`func=rhLambda,msg=${startMsg}`);
+
   const SM = new AWS.SecretsManager();
   if (!credentials) {
     try {
