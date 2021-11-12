e7b47f33efcb9ad5a8714a4978cb3a40bad7ffa4
GitThePower <henrypower11@gmail.com>
2021-11-11 23:22:47 -0500

    Setting log retention to one day for rh-lambda and updating cdk packages to most recent version

--- test/lib/stack.test.ts
+++ test/lib/stack.test.ts
@@ -17,12 +17,12 @@ beforeAll(() => {
 });
 
 test('Jastmm stack should create exactly these resources', () => {
-  // Number of resources at deploy should be this many -2 because
+  // Number of resources at deploy should be this many (12) + 2 because
   // CDK::Metadata and Cloudformation::Stack are not counted
-  expectCDK(stack).to(countResources("AWS::IAM::Policy", 1));
-  expectCDK(stack).to(countResources("AWS::IAM::Role", 1));
+  expectCDK(stack).to(countResources("AWS::IAM::Policy", 2));
+  expectCDK(stack).to(countResources("AWS::IAM::Role", 2));
   expectCDK(stack).to(countResources("AWS::Lambda::EventInvokeConfig", 1));
-  expectCDK(stack).to(countResources("AWS::Lambda::Function", 1));
+  expectCDK(stack).to(countResources("AWS::Lambda::Function", 2));
   expectCDK(stack).to(countResources("AWS::Lambda::Permission", 1));
   expectCDK(stack).to(countResources("AWS::SNS::Subscription", 1));
   expectCDK(stack).to(countResources("AWS::SNS::Topic", 1));
